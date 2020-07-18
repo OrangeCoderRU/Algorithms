25aab55930d9059b9b8a5df82673930f6b8ee198
Max Sablin <max.sablin@mail.ru>
2020-07-18 09:32:34 +0300

    Create binary_search.py

---
+++ binary_search.py
@@ -0,0 +1,18 @@
+def binary_search(list, item):
+    """Алгоритм бинарного поиска"""
+    low = 0
+    high = len(list) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        guess = list[int(mid)]
+        if guess == item: # совпадение
+            return mid
+        if guess > item: # уменьшаем максимум
+            high = mid - 1
+        else: # увеличиваем минимум
+            low = mid + 1
+    return None
+
+list = [5, 10, 15, 20, 25]
+
+print(binary_search(list, 20)) # 3
